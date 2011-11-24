82ea33d80544777dc92456a6a31865eac53c4d0d
Karsten Richter <kr@corika.com>
2011-11-24 11:22:30 +0100

    activate login requirement

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-#  before_filter :authenticate_user!#, :except => [:index]
+  before_filter :authenticate_user!#, :except => [:index]
 #  load_and_authorize_resource
   def index
   end
