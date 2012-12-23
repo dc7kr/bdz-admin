27add0ff7fbd9b5a8d15a8e02f01f27b03aee15f
Karsten Richter <kr@corika.com>
2012-12-23 12:19:06 +0100

    new letter event

--- app/models/member_event.rb
+++ app/models/member_event.rb
@@ -1,6 +1,17 @@
 class MemberEvent < ActiveRecord::Base
 	belongs_to :member
 
+	def self.newLetter(eventId,member,subject)
+		retval = MemberEvent.new
+		retval.member_id=member
+		retval.event_type="L"
+		retval.event_id=eventId
+		retval.event_date=Time.now
+		retval.comment=subject
+
+		retval
+	end
+
 	def self.newEmail(eventId,member,subject)
 		retval = MemberEvent.new
 		retval.member_id=member
