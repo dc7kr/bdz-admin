f7dcea38abc7a07aea65539a9f9419b46d81f5f7
Karsten Richter <kr@corika.com>
2013-11-12 08:44:44 +0100

    added num players to index view as well

--- app/controllers/festival_applications_controller.rb
+++ app/controllers/festival_applications_controller.rb
@@ -4,6 +4,7 @@ class FestivalApplicationsController < AuthenticatedController
   # GET /festival_applications.json
   def index
     @festival_applications = FestivalApplication.all
+    @sum_players = FestivalApplication.sum(:num_players)
 
     respond_to do |format|
       format.html # index.html.erb
