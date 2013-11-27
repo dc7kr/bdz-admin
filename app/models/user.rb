a08f9c76002564c84fbbaffc8ba0a3c957fbd1f5
Karsten Richter <kr@corika.com>
2013-11-27 17:09:17 +0100

    removed token_authenticable due to deprecation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable, :authentication_keys => [:login]
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
  
 
   # Setup accessible (or protected) attributes for your model
