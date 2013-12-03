5b98b450dbd53a03fac91ca9d3b7216290d024de
Karsten Richter <kr@corika.com>
2013-12-03 12:01:14 +0100

    added open air and regular participants to mail selection list

--- app/helpers/festival_mails_helper.rb
+++ app/helpers/festival_mails_helper.rb
@@ -4,6 +4,8 @@ module FestivalMailsHelper
     options_for_select([
       [t("festival_mail.groups.FA"),"FA"],
       [t("festival_mail.groups.FP"),"FP"], 
+      [t("festival_mail.groups.FR"),"FR"],
+      [t("festival_mail.groups.FO"),"FO"],
       [t("festival_mail.groups.FG"),"FG"],
       [t("festival_mail.groups.FJ"),"FJ"],
       [t("festival_mail.groups.FS"),"FS"]
