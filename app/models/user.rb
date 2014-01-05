874a6328ab96bde35d81bdc637a4f4ced767a635
Karsten Richter <kr@corika.com>
2014-01-05 18:10:38 +0100

    removed self registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
 
   
   validates :username,
