f6f0316a919355bbc7774c9e21bc338426c66b1b
Karsten Richter <kr@corika.com>
2014-01-27 21:34:23 +0100

    updated custom info mailer

--- app/mailers/custom_info_mail.rb
+++ app/mailers/custom_info_mail.rb
@@ -1,13 +1,24 @@
 class CustomInfoMail < ActionMailer::Base
   default from: "geschaeftsstelle@bdz-online.de"
 
-  def notify(recipient,email_params,attach_filename, attach_data)
+  def notify(recipient, subject, body, letter, attachment) 
 
-	  if ( attach_data) then
-		  attachments[attach_filename] = attach_data
+    storage_dir = BDZ_SETTINGS["invoice_archive_dir"]
+
+	  if ( letter ) then
+      letter_data = File.new(File.join(storage_dir,letter.filename)).read
+		  attachments["Anschreiben.pdf" ] = letter_data
     end
-	  @body = email_params[:body]
 
-   	mail(:to => recipient, :subject => email_params[:subject])
+    if (attachment) then
+      attachment_data = File.new(File.join(storage_dir,attachment.filename)).read
+		  attachments[attachment.orig_filename ] = attachment_data
+    else 
+      Rails.logger.info("No additional attachment")
+    end
+
+	  @body = body
+
+   	mail(:to => recipient, :subject => subject)
   end
 end
