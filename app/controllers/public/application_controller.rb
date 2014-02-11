0a0def887fe3e04c73cd4beb23091da07c9951fb
Karsten Richter <kr@corika.com>
2014-02-11 08:59:32 +0100

    added event_cards to public area, added views

---
+++ app/controllers/public/application_controller.rb
@@ -0,0 +1,3 @@
+class Public::ApplicationController  < ActionController::Base
+  layout "public"
+end
---
+++ app/controllers/public/event_cards_controller.rb
@@ -0,0 +1,23 @@
+class Public::EventCardsController < Public::ApplicationController
+
+  def order_form
+    @event_card = EventCard.new
+  end
+
+  def order_success
+    @event_card = EventCard.new(params[:event_card])
+
+    @event_card.orderdate = Time.now
+    respond_to do |format|
+      if @event_card.save
+        format.html 
+        format.json { render json: @event_card, status: :created, location: @event_card }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @event_card.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+end
+
+
