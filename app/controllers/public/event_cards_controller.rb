21a9039b3e8aef716bab937d246167ba672da4d5
Karsten Richter <kr@corika.com>
2014-02-18 09:34:26 +0100

    fix to form checking for event cards

--- app/controllers/public/event_cards_controller.rb
+++ app/controllers/public/event_cards_controller.rb
@@ -2,12 +2,12 @@ class Public::EventCardsController < Public::ApplicationController
 
   def order_form
     @event_card = EventCard.new
-
     @prices = BDZ_SETTINGS["festival_prices"]
   end
 
   def order_success
     @event_card = EventCard.new(params[:event_card])
+    @prices = BDZ_SETTINGS["festival_prices"]
 
     @event_card.orderdate = Time.now
     respond_to do |format|
@@ -17,7 +17,7 @@ class Public::EventCardsController < Public::ApplicationController
         format.html 
         format.json { render json: @event_card, status: :created, location: @event_card }
       else
-        format.html { render action: "new" }
+        format.html { render action: "order_form" }
         format.json { render json: @event_card.errors, status: :unprocessable_entity }
       end
     end
