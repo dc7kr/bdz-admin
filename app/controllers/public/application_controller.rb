17663fd993da73c3bb474fc4ad3732b2cb35c329
Karsten Richter <kr@corika.com>
2014-02-21 08:47:28 +0100

    - added locale detection to public controller (user preference) - added en translatoions for event cards views

--- app/controllers/public/application_controller.rb
+++ app/controllers/public/application_controller.rb
@@ -1,3 +1,13 @@
 class Public::ApplicationController  < ActionController::Base
+
+  before_filter :set_locale
+
   layout "public"
+
+  include ApplicationHelper
+
+ private
+    def set_locale
+      I18n.locale = http_accept_language.compatible_language_from(I18n.available_locales)
+    end
 end
