257cf23bfc2c00d9f5fceb0de067d36024e13c5a
Karsten Richter <kr@corika.com>
2014-05-06 00:01:56 +0200

    fixed potential NIL pointer in veg check

--- app/validators/meal_validator.rb
+++ app/validators/meal_validator.rb
@@ -9,6 +9,10 @@ class MealValidator < ActiveModel::EachValidator
         record.errors[attribute] << I18n.t("activerecord.errors.models.event_meal.attributes.tln.at_least_one")
       end
     elsif attribute == :veg then
+      if record.tln.nil? then
+        return
+      end
+
       unless value <= record.tln
         record.errors[attribute] << I18n.t("activerecord.errors.models.event_meal.attributes.veg.must_be_less_tln")
     end
