ba114747a5dcdc555befd1341527fd5137d18d78
Karsten Richter <kr@corika.com>
2014-05-10 16:18:21 +0200

    added reservation mails to festival_mails controller

--- app/controllers/festival_mails_controller.rb
+++ app/controllers/festival_mails_controller.rb
@@ -11,6 +11,25 @@ class FestivalMailsController < AuthenticatedNonResourceController
     end
   end
 
+  def reservation_invoices 
+    authorize! :member, :edit
+    respond_to do |format|
+      format.html
+    end
+  end
+  
+  def send_reservation_invoices
+    authorize! :member, :edit
+
+    EventCardInvoiceMailsWorker.perform_async(@current_user.id, "ECINVOICE")
+
+    respond_to do |format|
+        format.html { redirect_to home_festival_data_path, :notice => t('festival_mail.reservation_invoice_success') }
+    end
+  end
+
+
+
   def invoices
     authorize! :member, :edit
     respond_to do |format|
