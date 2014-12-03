8830f16318d1f411f03d50790f1a7cb2453c232b
Karsten Richter <kr@corika.com>
2014-12-03 18:33:31 +0100

    added missing mailer

---
+++ app/mailers/report_sheet_confirmation_mail.rb
@@ -0,0 +1,20 @@
+class ReportSheetConfirmationMail < ActionMailer::Base
+  default from: "geschaeftsstelle@bdz-online.de"
+
+  def notify(recipient, personalized_file, attachment_file, params) 
+
+    @rsi = params[:rsi]
+    @year = @rsi.report_sheet.year.to_s
+
+    @orchestra = @rsi.orchestra
+
+    @salutation = t('common.salutation_full.'+@orchestra.anrede)
+
+    if (personalized_file != nil ) then
+      attachment_data = File.new(personalized_file.full_path).read
+		  attachments[personalized_file.orig_filename ] = attachment_data
+    end
+
+   	mail(:to => recipient, :subject => "Meldebogen #{@year} erfolgreich eingereicht.")
+  end
+end
