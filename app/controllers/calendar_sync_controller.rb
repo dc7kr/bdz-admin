99403883988c04f94886d1bebd4d4e26eaef7abf
Karsten Richter <kasi@kasi-net.org>
2015-06-08 23:43:47 +0200

    updated BDZ-BW calendar sync
    - now sets date and time to nil and uses new datetime field

--- app/controllers/calendar_sync_controller.rb
+++ app/controllers/calendar_sync_controller.rb
@@ -20,8 +20,9 @@ class CalendarSyncController < AuthenticatedNonResourceController
 					conc.titel = summ[1]
 				end
 				conc.interpret = summ[0]
-				conc.datum = event.dtstart
-				conc.zeit = event.dtstart.localtime.strftime("%H:%M")
+				conc.datum = nil
+        conc.concert_date = event.dtstart
+				conc.zeit = nil 
 				loc = event.location.split(",")
 				conc.ort = loc[0]
 
