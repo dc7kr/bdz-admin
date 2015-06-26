9a3b23ffcb2ae90e24031a5b61b97ba5a57a562c
Karsten Richter <kasi@kasi-net.org>
2015-06-26 14:48:33 +0200

    improve git display

--- app/helpers/git_helper.rb
+++ app/helpers/git_helper.rb
@@ -6,10 +6,37 @@ module GitHelper
       		:environment => Rails.env,
       		:remote_url => `git remote -v`,
       		:remote_branch => `git branch -r`,
-      		:last_commit => `git log --max-count=5 `
+      		:last_commit => gitparse(`git log --max-count=5 `)
     	}
   		rescue
     	{}
   		end
 	end
+  private 
+  def gitparse(gitlines) 
+    commit_lines = gitlines.split("\n")
+    commit=nil
+
+    commits = Array.new
+    commit_lines.each do |c|
+      if c.start_with?("commit")
+        if not commit.nil?
+          commits << commit
+        end
+        commit = Hash.new
+        commit[:id] = c.gsub(/^commit\ +/,"")
+      elsif c.start_with?("Author:")
+        commit[:author]= c.gsub(/^Author:\ +/,"")
+      elsif c.start_with?("Date:")
+        commit[:date]= c.gsub(/^Date:\ +/,"")
+      else
+        if commit[:lines].nil? then
+          commit[:lines] = Array.new
+        end
+        commit[:lines] << c
+      end
+    end
+  
+    return commits
+  end
 end
