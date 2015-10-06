3817447505e10176b32c80be7c3a476a0d840191
Karsten Richter <kr@corika.com>
2015-10-06 16:41:32 +0200

    fix orchestra helper for link to orchestra_account

--- app/helpers/orchestras_helper.rb
+++ app/helpers/orchestras_helper.rb
@@ -2,8 +2,7 @@ module OrchestrasHelper
 
 	def link_to_orchestra_acct(orchestra, txt) 
     	if can? :update, orchestra
-                link_to image_tag('/assets/icons/account.png', {:size=>'16x16',:alt=>txt,:title=>txt,:class=>'btn'} ),orchestra_member_account_bookings_url(orchestra)
-
+        link_to content_tag(:span,"",:class=>"glyphicon glyphicon-euro"), orchestra_member_account_bookings_url(orchestra),:class =>"btn btn-xs btn-default"
     	end
 	end
 end
