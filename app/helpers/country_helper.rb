c2af90d4c91184a95cf046180b0b402265308484
Karsten Richter <kr@corika.com>
2015-10-08 10:53:07 +0200

    updated universitites view
    - new translated_state helper
    - added state to form and show view

--- app/helpers/country_helper.rb
+++ app/helpers/country_helper.rb
@@ -15,4 +15,28 @@ module CountryHelper
         return t_ctry
       end
   end
+
+  def translated_state(ccode,state,locale="de")
+    if state.nil? or ccode.nil? then
+      return ""
+    end
+    
+    ctry = ISO3166::Country[ccode]
+
+    tr_state  = ctry.states[state]
+
+    tr_state["name"]
+  end
+  def state_options_for_country(country_code)
+    country = ISO3166::Country[country_code]
+
+    states = Array.new
+
+    statearr = country.states.each do |id,data|
+      Rails.logger.debug(data)
+      states << [ data["name"],id ]
+    end
+
+    states
+  end
 end
