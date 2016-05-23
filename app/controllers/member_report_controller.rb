4f0f95d28fd15bd026158aaa18aac1356c8e55f3
Karsten Richter <kr@corika.com>
2016-05-23 16:43:26 +0200

    fix member report controller - new calculate() behaviour in Rails 4 - fix: remove grouping and use group() activerecord calls

--- app/controllers/member_report_controller.rb
+++ app/controllers/member_report_controller.rb
@@ -2,22 +2,23 @@ class MemberReportController < AuthenticatedNonResourceController
 
   def index
 	  authorize! :member, :edit
-	  @sums = ReportSheet.all(:select => "year, count(*) as anzahl, sum(report_sheets.children) as sum_children , sum(report_sheets.teens) as sum_teens, sum(report_sheets.youth) as sum_youth ,sum(report_sheets.adult) as sum_adult, sum(report_sheets.senior) as sum_senior , sum(report_sheets.azubi) as sum_azubi, sum(report_sheets.passive) as sum_passive", :order => "year", :group => "year")
+	  @sums = ReportSheet.select("year, count(*) as anzahl, sum(report_sheets.children) as sum_children , sum(report_sheets.teens) as sum_teens, sum(report_sheets.youth) as sum_youth ,sum(report_sheets.adult) as sum_adult, sum(report_sheets.senior) as sum_senior , sum(report_sheets.azubi) as sum_azubi, sum(report_sheets.passive) as sum_passive").group(:year).order(:year)
 
     @em_count = PersonMember.all.count
 
+    sheets = ReportSheet.includes(:orchestra).group(:year).order(:year)
 	
-	  @l_orch_no_pay = ReportSheet.includes(:orchestra).calculate(:sum,"children+teens+youth+adult+senior-azubi",:conditions => "orchestras.orch_type='L'",:group=>"year",:order=>"year")
-	  @l_orch_all= ReportSheet.includes(:orchestra).calculate(:sum,"children+teens+youth+adult+senior",:conditions => "orchestras.orch_type='L'",:group=>"year",:order=>"year")
+	  @l_orch_no_pay = sheets.calculate(:sum,"children+teens+youth+adult+senior-azubi",:conditions => "orchestras.orch_type='L'")
+	  @l_orch_all= sheets.calculate(:sum,"children+teens+youth+adult+senior",:conditions => "orchestras.orch_type='L'")
 
-	  @o_nomail = Orchestra.includes(:member).nomail.count()
-	  @em_nomail = PersonMember.includes(:member).nomail.count()
+	  @o_nomail = Orchestra.nomail.count()
+	  @em_nomail = PersonMember.nomail.count()
 	  @lorch = Hash.new
 
 	  @vers_sums = Array.new
 	  @vers_hash = Hash.new
 
-	  @uv_sum = ReportSheet.includes(:orchestra).calculate(:sum,"children+teens+youth+adult+senior",:group=>"year",:conditions=>"orchestras.orch_type<>'K' and uv=1",:order=>"year")
+	  @uv_sum = sheets.calculate(:sum,"children+teens+youth+adult+senior",:conditions=>"orchestras.orch_type<>'K' and uv=1")
 
 	  @uv_sum.each do |uv|
 		  @vers_sums.push(uv[0])
@@ -26,7 +27,7 @@ class MemberReportController < AuthenticatedNonResourceController
 		  @vers_hash[uv[0]]=h 
 	  end
 
-	  @haft_sum = ReportSheet.includes(:orchestra).calculate(:sum,"children+teens+youth+adult+senior",:group=>"year",:order=>"year",:conditions=>"orchestras.orch_type='O'")
+	  @haft_sum = sheets.calculate(:sum,"children+teens+youth+adult+senior",:conditions=>"orchestras.orch_type='O'")
 	  @haft_sum.each do |hv|
 		  vals = @vers_hash[hv[0]]
 		  if (vals != nil ) then
@@ -34,7 +35,7 @@ class MemberReportController < AuthenticatedNonResourceController
 		  end
 	  end
 
-	  @haft_sum = ReportSheet.includes(:orchestra).calculate(:sum,"children+teens+youth+adult+senior-azubi",:group=>"year",:order=>"year",:conditions=>"orchestras.orch_type='L'")
+	  @haft_sum = sheets.calculate(:sum,"children+teens+youth+adult+senior-azubi",:conditions=>"orchestras.orch_type='L'")
 
 
 	  @haft_sum.each do |hv|
@@ -46,7 +47,7 @@ class MemberReportController < AuthenticatedNonResourceController
 		  @lorch[lv[0]] = lv[1]
 	  end
 
-	  @orch_counts = Orchestra.count(nil,:group=>'orch_type')
+	  @orch_counts = Orchestra.group(:orch_type).count
 
 	  respond_to do |format|
       format.html
