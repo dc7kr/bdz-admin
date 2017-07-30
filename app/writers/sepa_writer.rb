d099557ae8523072af10a422564cdda2051811f3
Karsten Richter <kr@corika.com>
2017-07-30 11:31:39 +0200

    SEPA writer now supports credit transfer and direct debit variants

--- app/writers/sepa_writer.rb
+++ app/writers/sepa_writer.rb
@@ -1,33 +1,55 @@
 require 'sepa_tool'
 
 class SEPAWriter < BankTransferWriter
+  attr_accessor :direct_debits,:credit_transfers,:year,:tool
   
-  def initialize(datePrefix,settings,year=nil)
-    super(datePrefix)
+  def initialize(date_prefix,settings,year=nil)
+    super(date_prefix, settings['invoice_workdir']+"/")
+
     if year.nil? then
-      @year = Time.now.year
+      self.year = Time.now.year
     else
-      @year = year
+      self.year = year
     end
 
-    @tool = SEPATool.new(settings)
-    @direct_debits = Array.new
+    self.tool = SEPATool.new(settings)
+    self.direct_debits = Array.new
+    self.credit_transfers = Array.new
   end
 
-  private
-  def dd_from_member(member,seq_type=nil) 
-    dd = SepaDirectDebit.new(member,seq_type)
-    return dd
-  end
 
   public
-  def addBooking(customer, amount, remittance_txt,sequence_type=nil)
-    dd = dd_from_member(customer,sequence_type)
-    dd.remittance_txt = remittance_txt
-    dd.amount = amount 
+  def addDirectDebit(customer, amount, remittance_txt,sequence_type=nil)
+    if self.credit_transfers.count > 0 
+      false
+    else
+      dd = SepaDirectDebit.new(customer,sequence_type)
+      dd.remittance_txt = remittance_txt
+      dd.amount = amount 
 
-    Rails.logger.debug("New booking: #{customer.id}: #{dd.sequence_type}: #{amount}")
-    @direct_debits << dd
+      Rails.logger.debug("New booking: #{customer.id}: #{dd.sequence_type}: #{amount}")
+      self.direct_debits << dd
+
+      true
+    end
+  end
+
+  def addCreditTransfer(customer,remittance_txt, amount) 
+    if self.direct_debits.count > 0 
+      false
+    else
+      if not customer.is_direct_debit? then
+        Rails.logger.info("Customer #{customer.customer_id} is not considered for CreditTransfer - no IBAN/BIC!")
+        return false
+      end
+
+      ct = SEPACreditTransfer.new(customer,amount) 
+      ct.remittance_txt = remittance_txt
+
+      self.credit_transfers << ct
+      
+      true
+    end
   end
 
   def generateFile
@@ -39,18 +61,29 @@ class SEPAWriter < BankTransferWriter
   end
 
   def filename
-    @datePrefix+"_sepa.xml"
+    if self.direct_debits.count >0 
+      self.date_prefix+"_sepa_dd.xml"
+    elsif self.credit_transfers.count >0 
+      self.date_prefix+"_sepa_ct.xml"
+    else
+      nil
+    end
   end
 
   private
   def writeXml
-    if @direct_debits.count == 0 
+    if self.direct_debits.count == 0 and self.credit_transfers.count == 0
       return nil
     end
-    
-    sepaxml = @tool.create_sepa_direct_debit_order(@direct_debits)
 
-    outfile = MailingFile.new(self.filename,self.filename,@year.to_s)
+    sepaxml = nil
+    if self.direct_debits.count > 0 
+      sepaxml = self.tool.create_sepa_direct_debit_order(self.direct_debits)
+    elsif self.credit_transfers.count > 0 
+      sepaxml = self.tool.create_credit_transfer(self.credit_transfers)
+    end
+
+    outfile = MailingFile.new(self.filename,self.filename,self.year.to_s)
     sepaFile = File.open(outfile.full_path,"w")
     sepaFile << sepaxml
     sepaFile.close
