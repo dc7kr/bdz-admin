8a84133eb463a10877dcd666be6e62ca01296e06
Karsten Richter <kr@corika.com>
2017-12-18 12:05:51 +0100

    updated authorizers

--- app/authorizers/homepage_authorizer.rb
+++ app/authorizers/homepage_authorizer.rb
@@ -1,5 +1,4 @@
-class PublicEntityAuthorizer < ApplicationAuthorizer
-
+class HomepageAuthorizer < ApplicationAuthorizer
   # anyone can create a public entity
   def self.creatable_by?(user)
     true
@@ -10,15 +9,15 @@ class PublicEntityAuthorizer < ApplicationAuthorizer
   end
 
   def self.editable_by?(user)
-    user.has_role? :admin or user.has_role? :national
+    user.has_role? :admin
   end
 
   def self.updatable_by?(user)
-    user.has_role? :admin or user.has_role? :national
+    user.has_role? :admin
   end
 
   def self.deletable_by?(user) 
-    user.has_role? :admin or user.has_role? :national
+    user.has_role? :admin
   end
 
   def deletable_by?(user)
@@ -26,9 +25,9 @@ class PublicEntityAuthorizer < ApplicationAuthorizer
   end
 
   def editable_by?(user)
-    user.has_role? :admin or user.has_role? :national
+    user.has_role? :admin
   end
   def updatable_by?(user)
-    user.has_role? :admin or user.has_role? :national
+    user.has_role? :admin
   end
 end
