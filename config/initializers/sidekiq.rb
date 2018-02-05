dd2f7ab856922f9f9a41f5d7598378e8853f3c97
Karsten Richter <kr@corika.com>
2018-02-05 12:57:39 +0100

    remove credentials from sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,19 @@
+REDIS_SETTINGS = YAML.load_file(Rails.root.join('config', 'redis.yml'))[Rails.env]
+server=REDIS_SETTINGS['server']
+port=REDIS_SETTINGS['port']
+user=REDIS_SETTINGS['user']
+password=REDIS_SETTINGS['password']
+
+if user.nil? then
+  redis_url = "redis://#{server}:#{port}/12"
+else
+  redis_url = "redis://#{user}:#{password}@#{server}:#{port}/12"
+end
+
 Sidekiq.configure_server do |config|
-  config.redis = { :url => 'redis://user:***REMOVED***@***REMOVED***:6379/12', :namespace => 'bdz-'+Rails.env }
+  config.redis = { :url => redis_url, :namespace => 'bdz-'+Rails.env }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => 'redis://user:***REMOVED***@***REMOVED***:6379/12', :namespace => 'bdz-'+Rails.env }
+  config.redis = { :url => redis_url, :namespace => 'bdz-'+Rails.env }
 end
