7de79c2624d6c06b15041aabcd77505db9981d6f
Karsten Richter <kr@corika.com>
2018-02-05 12:54:31 +0100

    change to class based config

--- config/initializers/corika_invoices.rb
+++ config/initializers/corika_invoices.rb
@@ -1,2 +1,3 @@
-INVOICE_CONFIG= YAML.load_file(Rails.root.join('config', 'bdz-settings.yml'))[Rails.env]
-
+INVOICE_CONFIG_HASH= YAML.load_file(Rails.root.join('config', 'bdz-settings.yml'))[Rails.env]
+INVOICE_CONFIG= CorikaInvoices::Config.new(INVOICE_CONFIG_HASH['invoice_config'])
+INVOICE_CONTACT_HASH = INVOICE_CONFIG_HASH['contacts']
