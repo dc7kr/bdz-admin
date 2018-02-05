c2682887207b81c9ca211cdc3c35167de0c6afff
Karsten Richter <kr@corika.com>
2018-02-05 12:58:17 +0100

    add doc_config

--- config/initializers/bdz_settings.rb
+++ config/initializers/bdz_settings.rb
@@ -1,1 +1,3 @@
 BDZ_SETTINGS = YAML.load_file(Rails.root.join('config', 'bdz-settings.yml'))[Rails.env]
+APP_CONFIG_HASH= YAML.load_file(Rails.root.join('config', 'bdz-settings.yml'))[Rails.env]
+DOCS_CONFIG = DocumentsConfig.new(APP_CONFIG_HASH["documents"])
