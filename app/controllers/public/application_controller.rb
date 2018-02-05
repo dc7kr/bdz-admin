e55a1e3091c30cfe9fdbb4a6520d8c964823b43b
Karsten Richter <kr@corika.com>
2018-02-05 08:35:10 +0100

    change public controller to remove iframe restrictions

--- app/controllers/public/application_controller.rb
+++ app/controllers/public/application_controller.rb
@@ -1,10 +1,17 @@
 class Public::ApplicationController  < ActionController::Base
-
-  before_filter :set_locale
-
+  
+  include ApplicationHelper
+  
   layout "public"
 
-  include ApplicationHelper
+  before_filter :set_locale
+  after_filter :allow_iframe_requests
+
+  def allow_iframe_requests
+    response.headers.delete('X-Frame-Options')
+    #response.headers.except!  "X-Frame-Options"
+    Rails.logger.debug("filter x-frame-options public controller")
+  end
 
  private
     def set_locale
