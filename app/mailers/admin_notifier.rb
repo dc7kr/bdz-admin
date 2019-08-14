9d0dd309ff15ab13ff2e8cb039297dc41df11605
Karsten Richter <kr@corika.com>
2019-08-14 09:18:51 +0200

    add invoice_update and cleanup notify, coding convention cleanup

--- app/mailers/admin_notifier.rb
+++ app/mailers/admin_notifier.rb
@@ -1,6 +1,33 @@
 class AdminNotifier < ActionMailer::Base
   default from: "bdzdb@bdz-online.de"
 
+  def cleanup_notification(user, resigned_persons, resigned_orchestras)
+    @recipient = user
+    @resigned_persons = resigned_persons
+    @resigned_orchestras= resigned_orchestras
+
+    mail(:to => user.email, :subject => "Automatische Austritte")
+  end
+
+  def invoice_update(user, invoice, invoice_file, sepa_file, delta_amount, report_sheet)
+    @recipient = user
+    @invoice = invoice
+    @delta_amount = delta_amount
+    @report_sheet = report_sheet
+
+    @sepa_file = sepa_file
+
+    if not @sepa_file.nil? then
+      attachment_data = File.new(sepa_file.full_path).read
+		  attachments[sepa_file.orig_filename ] = attachment_data
+    end
+
+    invoice_data = File.new(invoice_file.full_path).read
+		attachments[invoice_file.orig_filename ] = invoice_data
+
+    mail(:to => user.email, :subject => "Rechnungs-Korrektur Mgl-Nr. "+invoice.customer.customer_id)
+  end
+
   def new_report_sheet(user,rs)
 	  @recipient = user
 	  @rs=rs
@@ -15,10 +42,10 @@ class AdminNotifier < ActionMailer::Base
 	 mail(:to => user.email, :subject => "Meldebogen Anschreiben")
   end
 
-  def newinvoices_notification(user, invoices, sepafile, current_user)
+  def newinvoices_notification(user, invoices, sepa_file, current_user)
 	 @recipient = user
 	 @invoice_url = invoices
-   @dd_url = sepafile		 
+   @dd_url = sepa_file		 
 
 	 @current_user = current_user 
 	 mail(:to => user.email, :subject => "BDZ Rechnungslauf")
@@ -42,16 +69,16 @@ class AdminNotifier < ActionMailer::Base
 	 mail(:to => user.email, :subject => "BDZ Mahnungslauf")
   end
   
-  def new_lv_ct_notification(user, sepafile, current_user)
+  def new_lv_ct_notification(user, sepa_file, current_user)
     @recipient = user
-    @sepafile_url = sepafile		 
+    @sepafile_url = sepa_file		 
 
     @current_user = current_user 
     mail(:to => user.email, :subject => "BDZ LV Beitragsanteile SEPA CT")
   end
   
-  def newdistinction_notification(invoice)
-    @sepafile_url = sepafile
+  def newdistinction_notification(invoice,sepa_file)
+    @sepafile_url = sepa_file
 
 	  @is_direct_debit = invoice.customer.is_direct_debit?
 
