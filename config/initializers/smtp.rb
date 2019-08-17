5df223042ebaf4a176a2f8ada5fa9b544fb4907c
Karsten Richter <kr@corika.com>
2019-08-17 13:06:39 +0200

    fix smtp initializer

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -5,12 +5,15 @@ yml = YAML.load_file(Rails.root.join('config', 'smtp.yml'))[Rails.env]
 smtp_options= yml["smtp"]
 url_options= yml["url"]
 
+BDZAdmin::Application.config.action_mailer.smtp_settings = {}
+BDZAdmin::Application.config.action_mailer.default_url_options = {}
+
 smtp_options.each do |name, value|
   BDZAdmin::Application.config.action_mailer.smtp_settings[name.to_sym] = value
-end unless options.nil?
+end unless smtp_options.nil?
 
 
 url_options.each do |name, value|
   BDZAdmin::Application.config.action_mailer.default_url_options[name.to_sym] = value
-end unless options.nil?
+end unless url_options.nil?
 end
