84ee83fae16a7c04f8f03ceb6f00b0dae92f3930
Karsten Richter <kr@corika.com>
2022-01-18 17:14:28 +0100

    new tariff correct job for EM (new discount for digital)

---
+++ app/jobs/em_tariff_correct_job.rb
@@ -0,0 +1,51 @@
+class EmTariffCorrectJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    digi_tariff = Tariff.where(tariff_type: 21).first
+    normal_tariff = Tariff.where(tariff_type: 20).first
+    student_tariff = Tariff.where(tariff_type: 25).first
+
+    changed=0
+    unchanged=0
+
+    digital = Array.new 
+    normal = Array.new
+
+    PersonMember.includes(:member).all.each do |em|
+      mail = em.member.email
+
+      if em.tariff_id == normal_tariff.id
+        if em.member.za=="L" and not mail.nil? and not mail.empty?
+          em.tariff = digi_tariff
+          em.save
+          digital << em
+          changed+=1
+        else
+          unchanged+=1
+        end
+      elsif em.tariff_id == digi_tariff.id
+        if em.member.za!= "L" or mail.nil? or mail.empty?
+          em.tariff = normal_tariff
+          em.save
+          normal << em
+          changed+=1
+        end
+      else
+        unchanged+=1
+      end
+    end
+
+    if changed > 0
+      users = User.for_admin_notify
+
+      users.each do |user|
+        AdminNotifier.em_tariff_fix_notification(user, digital, normal, changed, unchanged).deliver
+        logger.info 'Admin notify sent to %s' % user.email
+      end
+    else
+      logger.info("EM Tariff fix job had no changes.")
+    end
+  end
+end
+
