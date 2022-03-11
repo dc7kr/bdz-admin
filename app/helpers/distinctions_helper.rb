07fc70de98934be718b26ccf2fca348a91d1b970
Karsten Richter <kr@corika.com>
2022-03-11 08:44:40 +0100

    update distinctions helper to use FontAwesome

--- app/helpers/distinctions_helper.rb
+++ app/helpers/distinctions_helper.rb
@@ -1,10 +1,14 @@
 module DistinctionsHelper
 
   def link_to_generate_path(txt,path)
-    link_to image_tag('/assets/icons/new.png', {:size=>'16x16',:alt=>txt,:title=>txt,:class=>'btn'} ),path
+    link_to image_tag('/assets/icons/new.png', {:size=>'16x16',:alt=>txt,:title=>txt,:class=>'btn'} ),path, data: {tubolinks: false}
   end
 
-  def generate_button(txt,path) 
-    glyph_button("glyphicon-open-file", path,txt)
+  def generate_button(txt,path,entity) 
+    clazz = "btn-default"
+
+    link_to path, :class => "btn btn-default", data: { turbolinks: false} do 
+        content_tag(:span,"",:class=>"glyphicon glyphicon-open-file")+" #{txt}"
+    end
   end
 end
