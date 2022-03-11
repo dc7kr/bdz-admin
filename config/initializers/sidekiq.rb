772d984b13f1cce97ed3b55cb54d50d1cd8fe162
Karsten Richter <kr@corika.com>
2022-03-11 08:57:31 +0100

    add cron schedule to sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -17,3 +17,10 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { :url => redis_url, :namespace => 'bdz-'+Rails.env }
 end
+
+
+schedule_file = "config/schedule.yml"
+
+if File.exist?(schedule_file) && Sidekiq.server?
+  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+end
