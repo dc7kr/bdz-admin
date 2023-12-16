1e317c85a909adf78aa9a0cf7fdac54e414c4809
Karsten Richter <kr@corika.com>
2023-12-16 14:09:38 +0100

    new fee share class
    - convert share overview to new datastructure

---
+++ app/models/fee_shares.rb
@@ -0,0 +1,104 @@
+class FeeShares
+  attr_accessor :persons, :orchestras, :insurance, 
+    :direct_debit, :invoiced, :regional_organization, 
+    :pre_paid, :year, :leaf
+
+  def initialize(regional_organization: nil, year: Time.now.year, leaf: false)
+    
+    self.direct_debit = FeeShares.new(year: year, leaf: true) unless leaf
+    self.invoiced = FeeShares.new(year: year, leaf: true) unless leaf
+
+    self.leaf=leaf
+    self.year = year
+    self.insurance = 0
+    self.orchestras = 0 
+    self.persons = 0 
+    self.regional_organization = regional_organization
+
+    if not regional_organization.nil? and not regional_organization.member.nil?
+       self.pre_paid = regional_organization.member.member_account_bookings.where("booking_year = ? and booking_type= 'G'",year).sum(:amount)
+    end
+  end 
+
+  def leaf?
+    leaf
+  end
+
+  def total
+    if self.leaf?
+      persons+orchestras
+    else
+      self.direct_debit.total + self.invoiced.total
+    end
+  end
+
+  def insurance_total
+    if not self.leaf?
+      self.direct_debit.insurance + self.invoiced.insurance
+    else
+      insurance
+    end
+  end
+
+  def persons_total
+    if self.leaf
+      persons
+    else 
+      direct_debit.persons+invoiced.persons
+    end
+  end
+
+  def persons_share
+    persons_total * BDZ_SETTINGS['tariff']['lvPart']
+  end
+
+  def orchestras_total
+    if self.leaf
+      orchestras
+    else 
+      direct_debit.orchestras+invoiced.orchestras
+    end
+  end
+
+
+  def orchestras_share
+    orchestras_total * BDZ_SETTINGS['tariff']['lvPart']
+  end
+
+  def is_corrected?
+    uncorr = _share()
+
+    if uncorr  <  BDZ_SETTINGS['tariff']['lvMinimum']
+      true
+    else 
+      false
+    end
+  end
+
+  def _share(corrected = false)
+    percentage = BDZ_SETTINGS['tariff']['lvPart']
+
+    minimum = 0 
+
+    if corrected
+      minimum = BDZ_SETTINGS['tariff']['lvMinimum']
+    end
+
+    val = total * percentage
+    
+    if val < minimum
+      minimum
+    else 
+      val
+    end
+  end
+
+
+  def real_share
+    _share
+  end
+
+  def corrected_share
+    _share(true)
+  end
+end
