18d2dac2551168859c89fec6987b95631da4523b
Karsten Richter <kr@corika.com>
2024-01-11 13:22:02 +0100

    update magazine sampling
    - destroy contact when sampling is destroyed
    - add magazine address list row function for unified address list handling
    - introduce current_count to respect inactiv flag

--- app/models/magazine_sampling.rb
+++ app/models/magazine_sampling.rb
@@ -1,10 +1,30 @@
 class MagazineSampling < ApplicationRecord
   #attr_accessible :count
-  has_one :contact, as: :contact_entity
+  has_one :contact, as: :contact_entity, dependent: :destroy
   accepts_nested_attributes_for :contact
 
   def fullname
     contact.fullname
+  end
+
+  def magazine_address_list_row
+    if ( current_count >0) then
+      csvrow = {
+        :mglnr=>member.mglnr,
+        :name=> contact.company,
+        :name2=>contact.department,
+        :fullname=>contact.fullname,
+        :strasse=>contact.street,
+        :countryCode=>contact.countryCode,
+        :plz=>contact.zip,
+        :ort=>contact.city,
+        :land=>contact.letterCountry,
+        :magazines=>count
+      }
+      return csvrow
+    else
+      nil
+    end
   end
 
   def t_country
@@ -12,6 +32,14 @@ class MagazineSampling < ApplicationRecord
       contact.t_country("en")
     else
       ""
+    end
+  end
+
+  def current_count
+    if not inactive
+      count
+    else
+      0
     end
   end
 end
