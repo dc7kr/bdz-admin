a22793cc8ed5c6020e46ebe92849d5184f9d53bc
Karsten Richter <kr@corika.com>
2024-03-28 12:27:00 +0100

    update error mailer to use fixed address for sender

--- app/mailers/error_mailer.rb
+++ app/mailers/error_mailer.rb
@@ -4,14 +4,9 @@ class ErrorMailer < ActionMailer::Base
 
   def deliver_snapshot( exception, env, current_user)
 		@body = exception.to_s+"\n"+exception.backtrace.join("\n")
-      email = nil
-      if (current_user) then
-        email = current_user.email
-      else 
-        email = "bdzdb@bdz-online.de"
-      end
+      sender = "bdzdb@bdz-online.de"
 
       admin_mail = BDZ_SETTINGS['contacts']['admin']['mail']
-   		mail(:to => admin_mail, :subject => "Exception in "+env,:from=>email).deliver
+   		mail(:to => admin_mail, :subject => "Exception in "+env,:from=>sender).deliver
   end
 end
