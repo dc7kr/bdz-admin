868e830301f8df991b092db13f38b3f1b2c876ff
Karsten Richter <kr@corika.com>
2024-03-28 12:12:59 +0100

    new ApplicationJobs to replace workers

---
+++ app/jobs/generate_report_sheet_inputs_job.rb
@@ -0,0 +1,31 @@
+class GenerateReportSheetInputsJob < ApplicationJob
+  queue_as :default
+
+  after_perform do |job|
+    AdminNotifier.notify_generate_rsi(job.arguments)
+  end
+
+  def perform(year,user_id=nil)
+
+    @count = 0
+    @orchestras = Orchestra.regular.includes(:member)
+    
+    @orchestras.each do |o|
+      if not o.nil? and o.report_sheet_required? 
+        @rsi = ReportSheetInput.for_orchestra_and_year(o,year)
+
+        if @rsi.nil? then
+          @rsi = o.gen_rsi(year)
+          
+          @count+=1
+        end
+      else
+        if o.nil? then
+          logger.warn("Nil orchestra detected!!!")
+        else
+          logger.info("NO report sheet required: #{o.member.mglnr}") 
+        end
+      end
+    end
+  end
+end
---
+++ app/jobs/report_sheet_input_letter_job.rb
@@ -0,0 +1,15 @@
+require 'prawn' 
+require 'pdf/toolkit'
+
+class ReportSheetInputLetterJob < ApplicationJob
+  queue_as :default
+
+  def perform(orchestra_id,rsi_id)
+    orchestra = Orchestra.find(orchestra_id)
+    rsi = ReportSheetInput.find(rsi_id)
+
+    pdf = ReportSheetInputLetterPdf.new(orchestra,rsi)
+    pdf.generate
+  end
+end
+
