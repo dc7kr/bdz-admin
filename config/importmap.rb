91cd7f3fbcacaf68b796dd52a3482267e6e4d40a
Karsten Richter <kr@corika.com>
2024-04-10 08:57:09 +0200

    cleanup import map, fix order of imports

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,11 +1,14 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-
-pin "popper", to: "popper.js", preload: true
-pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "sidebar", to: "sidebar.js", preload: true
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+# Boilerplate ends
+
+pin "popper", to: "popper.js"
+pin "bootstrap", to: "bootstrap.min.js"
+pin "sidebar", to: "sidebar.js"
+pin "jquery" # @3.7.1
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
