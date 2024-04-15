75bbe472a9596f58c4288f40781f528e5c56834a
Karsten Richter <kr@corika.com>
2024-04-15 14:54:24 +0000

    update invoices initializer to cope with not existing config

--- config/initializers/corika_invoices.rb
+++ config/initializers/corika_invoices.rb
@@ -1,5 +1,9 @@
 Rails.application.config.to_prepare do
-  INVOICE_CONFIG_HASH= YAML.load_file(Rails.root.join('config', 'bdz-settings.yml'), aliases: true)[Rails.env]
-  INVOICE_CONFIG= CorikaInvoices::Config.new(INVOICE_CONFIG_HASH['invoice_config'])
-  INVOICE_CONTACT_HASH = INVOICE_CONFIG_HASH['contacts']
+  bdz_yaml = YAML.load_file(Rails.root.join('config', 'bdz-settings.yml'), aliases: true)
+
+  if not bdz_yaml.nil?
+    INVOICE_CONFIG_HASH= bdz_yaml[Rails.env]
+    INVOICE_CONFIG= CorikaInvoices::Config.new(INVOICE_CONFIG_HASH['invoice_config'])
+    INVOICE_CONTACT_HASH = INVOICE_CONFIG_HASH['contacts']
+  end
 end
