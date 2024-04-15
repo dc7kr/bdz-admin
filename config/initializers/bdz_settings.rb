c1cfcc81298497f98887664d08099d1de9b648ec
Karsten Richter <kr@corika.com>
2024-04-15 14:53:03 +0000

    fix bdz-settings initializer for precompile case (ignore non existing config)

--- config/initializers/bdz_settings.rb
+++ config/initializers/bdz_settings.rb
@@ -1,5 +1,9 @@
 Rails.application.config.to_prepare do
-  BDZ_SETTINGS = YAML.load_file(Rails.root.join('config', 'bdz-settings.yml'), aliases: true )[Rails.env]
-  APP_CONFIG_HASH= YAML.load_file(Rails.root.join('config', 'bdz-settings.yml'), aliases: true )[Rails.env]
-  DOCS_CONFIG = DocumentsConfig.new(APP_CONFIG_HASH["documents"])
+  bdz_yml = YAML.load_file(Rails.root.join('config', 'bdz-settings.yml'), aliases: true )
+ 
+  if not bdz_yml.nil?
+    BDZ_SETTINGS = bdz_yml[Rails.env]
+    APP_CONFIG_HASH= bdz_yml[Rails.env]
+    DOCS_CONFIG = DocumentsConfig.new(APP_CONFIG_HASH["documents"])
+  end
 end
