2ff528f8429dd098824ae08c095eef01d216ecce
Karsten Richter <kr@corika.com>
2024-04-26 12:22:02 +0000

    read revision from version yaml

--- config/initializers/revision.rb
+++ config/initializers/revision.rb
@@ -1,8 +1,11 @@
 Rails.application.config.to_prepare do
+  yml= YAML.load_file(Rails.root.join('config','version.yml'))
+  
+  GIT_REVISION =  yml["version_info"][:current]
+
   gh = GitTool.new
   GIT_INFO = gh.git_info(2)
 
   last_commit = GIT_INFO[:last_commits]
 
-  GIT_REVISION =  gh.current
 end
