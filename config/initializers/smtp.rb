3de3f08bc2b8fa28b76ca7929097bd714880dd19
Karsten Richter <kr@corika.com>
2024-04-26 12:20:36 +0000

    fix formatting

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,17 +1,17 @@
 unless BDZAdmin::Application.config.action_mailer.nil?
 
-smtp_options = Rails.application.credentials[:smtp]
-url_options = Rails.application.credentials[:url]
+  smtp_options = Rails.application.credentials[:smtp]
+  url_options = Rails.application.credentials[:url]
 
-BDZAdmin::Application.config.action_mailer.smtp_settings = {}
-BDZAdmin::Application.config.action_mailer.default_url_options = {}
+  BDZAdmin::Application.config.action_mailer.smtp_settings = {}
+  BDZAdmin::Application.config.action_mailer.default_url_options = {}
 
-smtp_options.each do |name, value|
-  BDZAdmin::Application.config.action_mailer.smtp_settings[name.to_sym] = value
-end unless smtp_options.nil?
+  smtp_options.each do |name, value|
+    BDZAdmin::Application.config.action_mailer.smtp_settings[name.to_sym] = value
+  end unless smtp_options.nil?
 
 
-url_options.each do |name, value|
-  BDZAdmin::Application.config.action_mailer.default_url_options[name.to_sym] = value
-end unless url_options.nil?
+  url_options.each do |name, value|
+    BDZAdmin::Application.config.action_mailer.default_url_options[name.to_sym] = value
+  end unless url_options.nil?
 end
