b6004dae144d9d3ae966976a9f7fc017442e8e18
Karsten Richter <kr@corika.com>
2024-06-24 08:05:29 +0000

    implement test notification views and controller

---
+++ app/controllers/adm/mail_check_controller.rb
@@ -0,0 +1,13 @@
+class Adm::MailCheckController < AuthenticatedNonResourceController
+
+  def index
+    authorize! :member, :edit
+  end
+
+  def admin_notify
+    authorize! :member, :edit
+
+    AdminNotifier.test_notification(current_user).deliver
+
+  end
+end
