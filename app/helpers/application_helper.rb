113ba77746dbd52a596b2a4449f63b9527ac2dfc
Karsten Richter <kr@corika.com>
2024-07-15 21:18:05 +0000

    update application table for dark sortable thead

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -162,7 +162,7 @@ end
 
 def sortable(column, title = nil)
   title ||= column.titleize
-  css_class = column == sort_column ? "sort current #{sort_direction}" : "sort"
+  css_class = column == sort_column ? "link-light sort current #{sort_direction}" : "link-light sort"
   direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
   link_to title , params.merge(:sort => column, :direction => direction, :page => nil).permit(:sort,:direction,:page), {:class => css_class }
 end
