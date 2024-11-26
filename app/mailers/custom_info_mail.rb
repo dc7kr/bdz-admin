82e2efb95b2a961df90a6a0d176cff65cc3e1698
Karsten Richter <kr@corika.com>
2024-11-26 16:37:37 +0000

    code cleanup

--- app/mailers/custom_info_mail.rb
+++ app/mailers/custom_info_mail.rb
@@ -1,11 +1,12 @@
 class CustomInfoMail < ApplicationMailer
+  default from: "geschaeftsstelle@zupfmusiker.de"
 
   def notify(recipient, letter, attachment, params) 
 
     @subject = params[:subject]
     @body = params[:body]
 
-	  if ( letter ) then
+    if ( letter ) then
       letter_data = File.new(letter.full_path).read
 		  attachments[letter.orig_filename ] = letter_data
     end
@@ -17,6 +18,6 @@ class CustomInfoMail < ApplicationMailer
       Rails.logger.info("No additional attachment")
     end
 
-   	mail(:to => recipient, :subject => @subject)
+    mail(:to => recipient, :subject => @subject)
   end
 end
