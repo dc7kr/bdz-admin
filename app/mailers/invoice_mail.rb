75c406be1de4b7cc44e3b258a6deeb51f3853111
Karsten Richter <kr@corika.com>
2024-11-26 16:38:06 +0000

    fix indentation, set default sender to geschaeftsstelle

--- app/mailers/invoice_mail.rb
+++ app/mailers/invoice_mail.rb
@@ -1,4 +1,5 @@
 class InvoiceMail < ApplicationMailer
+  default from: "geschaeftsstelle@zupfmusiker.de"
 
   def notify(recipient, invoice_file, attachment_file, params) 
 
@@ -10,12 +11,12 @@ class InvoiceMail < ApplicationMailer
     storage_dir = INVOICE_CONFIG.archive_dir
 
     attachment_data = File.new(invoice_file.full_path).read
-		attachments[invoice_file.orig_filename ] = attachment_data
+    attachments[invoice_file.orig_filename ] = attachment_data
 
-	  @recipient= recipient
+    @recipient = recipient
 
     @mglnr = mglnr
 
-   	mail(:to => recipient, :subject => subject)
+    mail(:to => recipient, :subject => subject)
   end
 end
