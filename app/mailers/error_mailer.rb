fea488da580934cd11b7ecdb0081f2da1cbc547a
Karsten Richter <kr@corika.com>
2024-11-26 16:38:54 +0000

    fix errormailer

--- app/mailers/error_mailer.rb
+++ app/mailers/error_mailer.rb
@@ -1,12 +1,10 @@
 class ErrorMailer < ApplicationMailer
-  default from: "bdzdb@zupfmusiker.de"
-
 
   def deliver_snapshot( exception, env, current_user)
-		@body = exception.to_s+"\n"+exception.backtrace.join("\n")
+      @body = exception.to_s+"\n"+exception.backtrace.join("\n")
       sender = "bdzdb@zupfmusiker.de"
 
       admin_mail = BDZ_SETTINGS['contacts']['admin']['mail']
-   		mail(:to => admin_mail, :subject => "Exception in "+env,:from=>sender).deliver
+      mail(:to => admin_mail, :subject => "Exception in "+env,:from=>sender).deliver_later
   end
 end
