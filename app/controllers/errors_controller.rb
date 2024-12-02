d5a89decffae8a10e89bcf191f46561051160391
Karsten Richter <kr@corika.com>
2024-12-02 08:07:24 +0000

    fix errors controller

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -8,21 +8,17 @@ class ErrorsController < ApplicationController
     #}, status: 404
   end
 
-  def internal_server_error
+  #def internal_server_error
     #render json: {
     #  status: 500,
     #  error: :internal_server_error,
     #  message: 'Houston we have a problem'
     #}, status: 500
-  end
+  #end
 
   def internal_server_error
-    if not error.nil?
-      logger.error("ERROR: "+error.to_s)
-      logger.error error.message + "\n " + error.backtrace.join("\n ")
-      respond_to do |format|
-        format.html {  render(:status=>500) }
-      end
+    respond_to do |format|
+      format.html {  render(:status=>500) }
     end
   end
 end
