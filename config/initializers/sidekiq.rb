1b9d655a63f7552dccd824b7ad60a632151ff3e7
Karsten Richter <kr@corika.com>
2024-12-20 14:16:39 +0000

    switch to REDIS_URL env var

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,6 @@
 Sidekiq.configure_server do |config|
+  config.redis = { :url => ENV.fetch("REDIS_URL") { "" } }
+
   config.on(:startup) do
     schedule_file = "config/schedule.yml"
     
@@ -6,9 +8,8 @@ Sidekiq.configure_server do |config|
         Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
     end   
   end    
-  config.redis = { :url => Rails.application.credentials.dig(:redis_url) }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => Rails.application.credentials.dig(:redis_url) }
+  config.redis = { :url => ENV.fetch("REDIS_URL") { "" } }
 end
