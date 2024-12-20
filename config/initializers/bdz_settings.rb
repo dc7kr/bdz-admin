a0c709b0835546442bb9438b60fc73f394cb57e6
Karsten Richter <kr@corika.com>
2024-12-20 14:57:31 +0000

    disable initializers conditionally to simplify precompile runs

--- config/initializers/bdz_settings.rb
+++ config/initializers/bdz_settings.rb
@@ -1,9 +1,11 @@
-Rails.application.config.to_prepare do
-  bdz_yml = YAML.load_file(Rails.root.join('config', 'bdz-settings.yml'), aliases: true )
- 
-  if not bdz_yml.nil?
-    BDZ_SETTINGS = bdz_yml[Rails.env]
-    APP_CONFIG_HASH= bdz_yml[Rails.env]
-    DOCS_CONFIG = DocumentsConfig.new(APP_CONFIG_HASH["documents"])
+if ENV['ASSET_PRECOMPILE'].blank?
+  Rails.application.config.to_prepare do
+    bdz_yml = YAML.load_file(Rails.root.join('config', 'bdz-settings.yml'), aliases: true )
+   
+    if not bdz_yml.nil?
+      BDZ_SETTINGS = bdz_yml[Rails.env]
+      APP_CONFIG_HASH= bdz_yml[Rails.env]
+      DOCS_CONFIG = DocumentsConfig.new(APP_CONFIG_HASH["documents"])
+    end
   end
 end
