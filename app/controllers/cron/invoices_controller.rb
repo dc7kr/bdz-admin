e083d0c71018b9806099f21afd1300cb3ccd34ab
Karsten Richter <kr@corika.com>
2025-01-24 11:01:28 +0000

    remove dead code from cron invoices controller

--- app/controllers/cron/invoices_controller.rb
+++ app/controllers/cron/invoices_controller.rb
@@ -49,7 +49,7 @@ class Cron::InvoicesController < AuthenticatedNonResourceController
     PersonMemberInvoicesJob.perform_later(year, @current_user.id)
 
     respond_to do |format|
-      format.html { redirect_to home_cron_path, notice: t('cron.person_member_invoice_success') }
+      format.html { redirect_to home_cron_path, notice: t('cron.invoice_person_member_success') }
     end
   end
 
@@ -57,56 +57,6 @@ class Cron::InvoicesController < AuthenticatedNonResourceController
     authorize! :member, :edit
     testGen(params[:date])
     render text: 'Generation OK.'
-  end
-
-  # manual query respecting already invoiced:
-  # SELECT m.id, m.strasse, mb.id from person_members m LEFT JOIN member_account_bookings mb ON m.member_id=mb.member_id AND mb.booking_type='B' WHERE mb.id is not null and year(mb.booking_date)=year(now())
-
-  # query for already existing bookings
-  # SELECT m.id, m.strasse, mb.id FROM person_members m LEFT JOIN member_account_bookings mb ON m.member_id = mb.member_id AND mb.booking_type = 'B' WHERE mb.id IS NOT NULL AND year( mb.booking_date ) = year( now( ) )
-
-  def personMemberInvoices(year)
-    @person_members = PersonMember.includes(%i[tariff
-                                               member]).joins("LEFT JOIN member_account_bookings mb ON person_members.member_id=mb.member_id AND mb.booking_type='B' and YEAR(mb.booking_date) = YEAR(NOW())").where('mb.id IS NULL').order('members.mglnr')
-
-    @direct_debits = []
-
-    @tw = TexWriter.new
-
-    datePrefix = Time.now.strftime '%Y%m%d%H%M%S'
-    @sw = CorikaInvoices::SepaWriter.new(datePrefix, INVOICE_CONFIG)
-
-    @person_members.each do |person|
-      next if person.tariff.amount == 0
-
-      @cur_year = Time.now.year
-      @booking_txt = 'Beitrag ' + person.tariff.description + ' ' + String(@cur_year)
-      @booking = MemberAccountBooking.newInvoice(@booking_txt, -1 * person.tariff.amount, String(person.mglnr))
-      @booking.member_id = person.id
-      @booking.save
-
-      @tw.write(person, year)
-      @tw.writePersonTariff(person)
-      system('/opt/bdz-rechnung/bin/rechnung.sh ' + String(person.mglnr))
-
-      next unless person.is_direct_debit?
-
-      remittance_txt = 'BDZ-Beitrag ' + year.to_s + ' ' + person.mglnr.to_s
-      @sw.addDirectDebit(person, person.tariff.amount, remittance_txt)
-
-      @booking = MemberAccountBooking.newWithdrawal('Lastschrift ' + @booking_txt, person.tariff.amount)
-      @booking.member_id = person.id
-      @booking.save
-    end
-
-    @invoice_type = 'rechnung-em'
-    system('/opt/bdz-rechnung/bin/merge_pdfs.sh rechnung ' + @invoice_type)
-
-    @ddFile = @sw.generateFile
-    @date_prefix = @sw.datePrefix
-
-    @tw.moveGeneratedFiles(@date_prefix)
-    send_mail(@ddFile, @invoice_type)
   end
 
   def testGen(datepref)
