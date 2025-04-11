72abf933d879fe0068ef370888716335429d22bb
Karsten Richter <kr@corika.com>
2025-04-11 08:48:09 +0000

    remove class member in favor of class method

--- app/models/orchestra_contact.rb
+++ app/models/orchestra_contact.rb
@@ -2,14 +2,13 @@ class OrchestraContact < ApplicationRecord
   belongs_to :orchestra
 
   validates :email, email_format: true
-  @@roles = %w[V S G D J O R Z]
 
   def self.permitted_params
     %i[role salutation first_name last_name street zip city country_code email phone]
   end
 
   def self.roles
-    @@roles
+    %w[V S G D J O R Z]
   end
 
   def fullname
