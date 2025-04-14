4752dec52ab0f2c6e5c349e50ee9c143edad0338
Karsten Richter <kr@corika.com>
2025-04-14 06:37:18 +0000

    add non-parallel capsule, fix rubocop findings

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,19 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL') { '' } }
+  config.redis = { url: ENV.fetch("REDIS_URL") { "" } }
+
+  config.capsule("serialized") do |cap|
+    cap.concurrency = 1
+    cap.queues = %w[queue_a queue_b] # strict priority
+    # cap.queues = %w[queue_a,3 queue_b,1] # weighted
+  end
 
   config.on(:startup) do
-    schedule_file = 'config/schedule.yml'
+    schedule_file = "config/schedule.yml"
 
     Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file) && Sidekiq.server?
   end
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL') { '' } }
+  config.redis = { url: ENV.fetch("REDIS_URL") { "" } }
 end
