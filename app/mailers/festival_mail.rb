3a523cc28c4a1ab425cb83d91518f44c48cc74a5
Karsten Richter <kr@corika.com>
2025-05-27 12:15:45 +0000

    fix festival mail class name

--- app/mailers/festival_mail.rb
+++ app/mailers/festival_mail.rb
@@ -1,4 +1,4 @@
-class FestivalMailMailer < ApplicationMailer
+class FestivalMail < ApplicationMailer
   default from: 'info@eurofestival-zupfmusik.de'
 
   def notify(recipient, personalized_file, attachment_file, params)
