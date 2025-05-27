64b0f775cdd9479b3f0edc25516a0a1d4304aee5
Karsten Richter <kr@corika.com>
2025-05-27 12:02:41 +0000

    update festival mails job to use current festival data, internalize essential helper functions

--- app/jobs/festival_mails_job.rb
+++ app/jobs/festival_mails_job.rb
@@ -6,12 +6,13 @@ class FestivalMailsJob < ApplicationJob
   include UploadHelper
   include FestivalMailsHelper
 
-  def perform(_user_id, letterfile_hash, attachment_hash, subject, body_template, event_id, group, via_paper)
+
+  def perform(_user_id, mail_params)
     successCount = 0
     failCount = 0
 
     letterfile = MailingFile.from_hash(letterfile_hash)
-    attachment = MailingFile.from_hash(attachment_hash)
+    attachment = MailingFile.from_hash(mail_params[:datafile])
 
     cur_year = Time.zone.now.year
 
@@ -23,24 +24,19 @@ class FestivalMailsJob < ApplicationJob
 
     case group
     when 'FA'
-      applicants = FestivalApplication.current_festival.includes(:contact_person)
+      applicants = FestivalApplication.current_with_contacts
     when 'FP'
-      applicants = FestivalApplication.current_festival.includes(:contact_person).where(permission: true)
+      applicants = FestivalApplication.current_with_contacts.where(permission: true)
     when 'FR'
-      applicants = FestivalApplication.current_festival.includes(:contact_person).where(permission: true,
-                                                                                        visitor_type: 'R')
+      applicants = FestivalApplication.current_with_contacts.where(permission: true, visitor_type: 'R')
     when 'FS'
-      applicants = FestivalApplication.current_festival.includes(:contact_person).where(permission: true,
-                                                                                        visitor_type: 'V')
+      applicants = FestivalApplication.current_with_contacts.where(permission: true, visitor_type: 'V')
     when 'FJ'
-      applicants = FestivalApplication.current_festival.includes(:contact_person).where(permission: true,
-                                                                                        visitor_type: 'Y')
+      applicants = FestivalApplication.current_with_contacts.where(permission: true, visitor_type: 'Y')
     when 'FG'
-      applicants = FestivalApplication.current_festival.includes(:contact_person).where(permission: true,
-                                                                                        visitor_type: 'G')
+      applicants = FestivalApplication.current_with_contacts.where(permission: true, visitor_type: 'G')
     when 'FO'
-      applicants = FestivalApplication.current_festival.includes(:contact_person).where(permission: true,
-                                                                                        visitor_type: 'O')
+      applicants = FestivalApplication.current_with_contacts.where(permission: true, visitor_type: 'O')
     else
       logger.error("NO GROUP identified: #{group}")
     end
@@ -75,4 +71,35 @@ class FestivalMailsJob < ApplicationJob
 
     send_admin_mail(pdf_merged_file, triggered_by, results)
   end
+
+  private
+  def replace_body(orig_body, subst)
+    body = orig_body
+    subst.each do |s|
+      # logger.debug("Subst-Pattern:"+s[0])
+      body = body.gsub(s[0], s[1])
+    end
+
+    body
+  end
+
+  def prepare_body(appl, body)
+    @festival_concert = appl.festival_concert
+
+    substitutes = {
+      '%id%' => appl.id.to_s,
+      '%teilnehmer_name%' => appl.orch_name
+    }
+
+    substitutes['%probenzeit%'] = appl.rehearsal_time.to_s unless appl.rehearsal_time.nil?
+
+    unless @festival_concert.nil?
+      substitutes['%konzert%'] = @festival_concert.label.to_s
+      substitutes['%konzert_zeit%'] = I18n.l(@festival_concert.event_time)
+      substitutes['%konzert_ort%'] = @festival_concert.location
+    end
+
+    replace_body(body, substitutes)
+  end
+
 end
