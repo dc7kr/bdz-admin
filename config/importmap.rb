eabf3a46e27bc3ef4ca6e7ed6850df42aae22c0c
Karsten Richter <kr@corika.com>
2025-06-04 16:46:06 +0000

    cleanup and update importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,18 +1,29 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
+pin "application", preload: true
+
+#don't put the actual URLs here - use bin/importmap update / outdated
+
+pin "@rails/actioncable", to: "actioncable.esm.js"
+pin "@rails/activestorage", to: "activestorage.esm.js"
+pin "@rails/actiontext", to: "actiontext.esm.js"
+
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@rails/actioncable", to: "actioncable.esm.js"
 pin_all_from "app/javascript/channels", under: "channels"
+
 # Boilerplate ends
 
-pin "popper", to: "popper.js"
-pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "sidebar", to: "sidebar.js"
+# we explicitly use bootstrap from the gem for consistency
+pin "popper", to: 'popper.js', preload: true
+pin "bootstrap", to: 'bootstrap.min.js', preload: true
+
+
+pin "sidebar", to: "sidebar.js", preload: true
+
 pin "jquery" # @3.7.1
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
 
 # DONT ADD FONT AWESOME HERE
