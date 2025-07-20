abca4c08a0b32bc15133a2e76dff5ebd96d3824d
Karsten Richter <kr@corika.com>
2025-07-20 10:39:30 +0000

    fix host name for dev instance

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,7 +46,8 @@ Rails.application.configure do
    # caching is enabled.
    config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+
+  config.action_mailer.default_url_options = { host: "admin-dev.zupfmusiker.de" }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
