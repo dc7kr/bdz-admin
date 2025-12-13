c4cb4ccf04028c8ede670d5017a52b6c49045330
Karsten Richter <kr@corika.com>
2025-12-13 10:33:06 +0000

    enforce presence of mandatory fields

--- app/models/festival_piece.rb
+++ app/models/festival_piece.rb
@@ -2,4 +2,6 @@ class FestivalPiece < ApplicationRecord
   # attr_accessible :composer, :duration, :festival_application, :title
 
   belongs_to :festival_application
+
+  validates_presence_of :title, :composer, :duration, :publisher
 end
