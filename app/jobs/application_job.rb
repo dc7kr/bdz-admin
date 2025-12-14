daf1382356e6e50cc555121b1bd28727bdbf093c
Karsten Richter <kr@corika.com>
2025-12-14 12:00:20 +0000

    remove default_url_options as it breaks festival mails job

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,12 +1,6 @@
 class ApplicationJob < ActiveJob::Base
-  include Rails.application.routes.url_helpers
+  #include Rails.application.routes.url_helpers
   # Automatically retry jobs that encountered a deadlock
   # retry_on ActiveRecord::Deadlocked
 
-  # Most jobs are safe to ignore if the underlying records are no longer available
-  # discard_on ActiveJob::DeserializationError
-  protected
-  def default_url_options
-    Rails.application.config.active_job.default_url_options
-  end
 end
