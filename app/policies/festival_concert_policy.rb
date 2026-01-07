6ecc252ec7b0275a0cc249a4cec0c51a574eea66
Karsten Richter <kr@corika.com>
2026-01-07 17:15:44 +0000

    fix festival concert and exhibitor policies

--- app/policies/festival_concert_policy.rb
+++ app/policies/festival_concert_policy.rb
@@ -1,5 +1,13 @@
 class FestivalConcertPolicy < FestivalDataPolicy
   def programme? 
-    national_permission? or user.has_role :festival
+    national_permission? or user.has_role? :festival
+  end
+
+  class Scope < ApplicationPolicy::Scope
+    def resolve
+      if national_permission? or user.has_role? :festival
+        scope.all
+      end
+    end
   end
 end
--- app/policies/festival_exhibitor_policy.rb
+++ app/policies/festival_exhibitor_policy.rb
@@ -1,2 +1,18 @@
 class FestivalExhibitorPolicy < FestivalDataPolicy
+
+  def show?
+    super or user.has_role? :festival
+  end
+
+  def invoice_preview?
+    national_permission?
+  end
+
+  class Scope < FestivalDataPolicy::Scope
+    def resolve
+      if national_permission? or user.has_role? :festival
+        scope.all
+      end
+    end
+  end
 end
