08210fd7130d1f660fd3fdf04b40b0782a4fd8cb
Karsten Richter <kr@corika.com>
2026-01-07 14:16:27 +0000

    change eror backtrace helper

--- app/helpers/errors_helper.rb
+++ app/helpers/errors_helper.rb
@@ -1,5 +1,9 @@
 module ErrorsHelper
   def filter_backtrace(bt)
-    bt.grep_v(/.bundle/).join(raw("<br>"))
+    #bt.grep_v(/.bundle/).join(raw("<br>"))
+    bt
+  end
+  def find_our_first(bt)
+    bt.grep_v(/.bundle/)[0]
   end
 end
