6f20a90cd524ea50debe933f9462dc84153aecf3
Karsten Richter <kr@corika.com>
2026-01-15 14:30:09 +0000

    fix member report policy scope and authorization

--- app/controllers/member_report_controller.rb
+++ app/controllers/member_report_controller.rb
@@ -1,18 +1,18 @@
 class MemberReportController < AuthenticatedNonResourceController
   def index
-    authorize! :member, :edit
-    @sums = ReportSheet.select("year, count(*) as anzahl, sum(report_sheets.children) as sum_children , sum(report_sheets.teens) as sum_teens, sum(report_sheets.youth) as sum_youth ,sum(report_sheets.adult) as sum_adult, sum(report_sheets.senior) as sum_senior , sum(report_sheets.azubi) as sum_azubi, sum(report_sheets.passive) as sum_passive, sum(child_ens) as sum_child_ens, sum(youth_ens) as sum_youth_ens, sum(adult_ens) as sum_adult_ens, sum(senior_ens) as sum_senior_ens, sum(chamber_ens) as sum_chamber_ens, sum(other_ens) as sum_other_ens").group(:year).order(:year)
+    auhorize :member_data, :show
+    @sums = policy_scope(ReportSheet).select("year, count(*) as anzahl, sum(report_sheets.children) as sum_children , sum(report_sheets.teens) as sum_teens, sum(report_sheets.youth) as sum_youth ,sum(report_sheets.adult) as sum_adult, sum(report_sheets.senior) as sum_senior , sum(report_sheets.azubi) as sum_azubi, sum(report_sheets.passive) as sum_passive, sum(child_ens) as sum_child_ens, sum(youth_ens) as sum_youth_ens, sum(adult_ens) as sum_adult_ens, sum(senior_ens) as sum_senior_ens, sum(chamber_ens) as sum_chamber_ens, sum(other_ens) as sum_other_ens").group(:year).order(:year)
 
-    @em_count = PersonMember.count
+    @em_count = policy_scope(PersonMember).count
 
-    sheets = ReportSheet.includes(:orchestra).group(:year).order(:year)
+    sheets = policy_scope(ReportSheet).includes(:orchestra).group(:year).order(:year)
 
     @l_orch_no_pay = sheets.where("orchestras.orch_type='L'").calculate(:sum,
                                                                         "children+teens+youth+adult+senior-azubi")
     @l_orch_all = sheets.where("orchestras.orch_type='L'").calculate(:sum, "children+teens+youth+adult+senior")
 
-    @o_nomail = Orchestra.nomail.count
-    @em_nomail = PersonMember.nomail.count
+    @o_nomail = policy_scope(Orchestra).nomail.count
+    @em_nomail = policy_scope(PersonMember).nomail.count
     @lorch = {}
 
     @vers_sums = []
@@ -42,7 +42,7 @@ class MemberReportController < AuthenticatedNonResourceController
       @lorch[lv[0]] = lv[1]
     end
 
-    @orch_counts = Orchestra.group(:orch_type).count
+    @orch_counts = policy_scope(Orchestra).group(:orch_type).count
 
     respond_to do |format|
       format.html
@@ -53,23 +53,22 @@ class MemberReportController < AuthenticatedNonResourceController
   end
 
   def by_lv
-    authorize! :member, :edit
     if params[:year].nil?
       Time.zone.now.year.to_s
     else
       params[:year]
     end
 
-    @sums = ReportSheet.query("SELECT rs.*,m.regional_organization_id from report_sheets rs,members m where rs.orchestra_id=m.id and year=? GROUP BY m.regional_organization_id")
+    @sums = policy_scope(ReportSheet).query("SELECT rs.*,m.regional_organization_id from report_sheets rs,members m where rs.orchestra_id=m.id and year=? GROUP BY m.regional_organization_id")
 
-    @sums = ReportSheet.includes(%i[orchestra member]).all(
+    @sums = policy_scope(ReportSheet).includes(%i[orchestra member]).all(
       select: "year, count(*) as anzahl, sum(report_sheets.children) as sum_children , sum(report_sheets.teens) as sum_teens, sum(report_sheets.youth) as sum_youth ,sum(report_sheets.adult) as sum_adult, sum(report_sheets.senior) as sum_senior , sum(report_sheets.azubi) as sum_azubi, sum(report_sheets.passive) as sum_passive", order: "member.regional_organization", group: "year"
     )
   end
 
   def report_sheet_stats
     authorize! :member, :edit
-    sheets = ReportSheet.final(Time.zone.now.year).includes(:orchestra)
+    sheets = policy_scope(ReportSheet).final(Time.zone.now.year).includes(:orchestra)
 
     @maxTariff = 0
     @minTariff = 0
