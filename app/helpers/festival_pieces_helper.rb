16b7a52eaecb68a3b7f70a308fb6ab1882a55ef4
Karsten Richter <kr@corika.com>
2026-01-25 15:16:28 +0000

    remove unused render_duration method

--- app/helpers/festival_pieces_helper.rb
+++ app/helpers/festival_pieces_helper.rb
@@ -2,14 +2,4 @@ module FestivalPiecesHelper
   def festival_pieces_count
     pluralize(FestivalPiece.count, "comment")
   end
-
-  def render_duration(duration_time)
-    begin
-      duration_time.nil? ? "" : duration_time.strftime("%H:%M")
-    rescue => e
-      "INVALID"
-      Rails.logger.error e.message
-      Rails.logger.error e.backtrace.join("\n")
-    end
-  end
 end
