a22a8ddbbaad2f4e7870339414c26a945c13234a
Karsten Richter <kr@corika.com>
2026-01-25 16:52:08 +0000

    add no_tickets to routes

--- config/routes.rb
+++ config/routes.rb
@@ -146,6 +146,7 @@ Rails.application.routes.draw do
       post :grp_list
       get :list
       get :permitted
+      get :no_tickets
       get :participant_overview
       get :gen_participant_sheets
       get :open_issues
