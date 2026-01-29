7727def92d707df603657ba02b786190c02006e0
Karsten Richter <kr@corika.com>
2026-01-29 14:53:30 +0000

    explicitly add helpers as helpers to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  helper ButtonHelper
+  helper NavHelper
+  helper FontAwesomeHelper
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
