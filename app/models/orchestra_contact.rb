bd38641fe096dcbfbcedcff71b3046de79663346
Karsten Richter <kr@corika.com>
2026-02-10 17:32:38 +0000

    add to_hash to orchestra contact

--- app/models/orchestra_contact.rb
+++ app/models/orchestra_contact.rb
@@ -15,6 +15,21 @@ class OrchestraContact < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
+  def to_hash
+    hash = {}
+    hash["salutation"] = salutation
+    hash["first_name"] = first_name
+    hash["last_name"] = last_name
+    hash["street"] = street
+    hash["zip"] = zip
+    hash["city"] = city
+    hash["role"] = role
+    hash["email"] = email
+    hash["phone"] = phone
+
+    hash
+  end
+
   def to_s
     data = [ "#{first_name} #{last_name}", street, "#{zip} #{city}", phone, email ]
     data.join("\n")
