d882921b10de39f13f7a7ae009fa01663e2f5ff0
Karsten Richter <kr@corika.com>
2026-02-12 10:06:03 +0000

    fix exchange link for orchestra member helper

--- app/helpers/orchestra_members_helper.rb
+++ app/helpers/orchestra_members_helper.rb
@@ -2,7 +2,8 @@ module OrchestraMembersHelper
   def link_to_exchange(_path, _txt, entity)
     return unless policy(entity).update?
 
-    link_to content_tag(:span, "", class: "glyphicon glyphicon-transfer"), { id: entity, action: "exchange" },
-            class: "btn btn-xs btn-default"
+    link_to _path, class: "btn btn-secondary btn-sm" do
+      my_fa_icon("exchange-alt")
+    end
   end
 end
