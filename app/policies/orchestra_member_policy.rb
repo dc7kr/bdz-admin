8531b29b27703ff2feead66031062d2c71bc3c0e
Karsten Richter <kr@corika.com>
2026-02-16 12:01:32 +0000

    enable distinction users to read orchestra_members

--- app/policies/orchestra_member_policy.rb
+++ app/policies/orchestra_member_policy.rb
@@ -1,7 +1,23 @@
 class OrchestraMemberPolicy < MemberDataPolicy
 
   def exchange?
-    national_permission
+    national_permission?
+  end
+
+  def index
+    super or user.has_role? :distinction
+  end
+
+  def show
+    super or user.has_role? :distinction
+  end
+
+  class Scope < ApplicationPolicy::Scope
+    def resolve
+      if national_permission? or user.has_role? :distinction
+        scope.all
+      end
+    end
   end
 
 end
