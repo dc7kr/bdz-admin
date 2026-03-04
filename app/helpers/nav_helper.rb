010f6a0b3c4f8799df9048981390adccf97f59fd
Karsten Richter <kr@corika.com>
2026-03-04 16:38:37 +0000

    fix nav_to_edit for non-id entities

--- app/helpers/nav_helper.rb
+++ app/helpers/nav_helper.rb
@@ -1,6 +1,6 @@
 module NavHelper
   def nav_to_edit(entity, path = nil)
-    return if not entity.present? 
+    return if not entity.present?
 
     if entity.is_a?(Array)
       namespace = entity[0]
@@ -10,16 +10,16 @@ module NavHelper
       path = send("edit_#{namespace}_#{entity.class.name.singularize.underscore}_path",entity) if path.nil?
     else
       return unless entity.id.present?
-      path = send("edit_#{entity.class.name.singularize.underscore}_path",entity.id) if path.nil?
+      path = send("edit_#{entity.class.name.singularize.underscore}_path",entity) if path.nil?
     end
-      
+
     return if entity.nil? or entity.id.nil?
 
     return unless policy(entity).update?
 
     path = { action: "edit", controller: entity.class.name.underscore.pluralize, id: entity }  if path.nil?
 
-    content_tag(:li, class: "nav-item") do 
+    content_tag(:li, class: "nav-item") do
       link_to path, :class => tabActiveClass(action_name,'edit',"nav-link") do
         concat(my_fa_icon("edit"))
         concat("&nbsp;".html_safe)
@@ -43,7 +43,7 @@ module NavHelper
   end
 
   def nav_to_show(entity, path = nil)
-    
+
     return if not entity.present? or not entity.id.present?
 
     if entity.is_a?(Array)
@@ -59,7 +59,7 @@ module NavHelper
     path = { action: "show", controller: entity.class.name.underscore.pluralize } if path.nil?
 
     content_tag(:li, class: "nav-item") do
-      link_to path, class: tabActiveClass(action_name, "show", "nav-link") do 
+      link_to path, class: tabActiveClass(action_name, "show", "nav-link") do
         concat(my_fa_icon("eye"))
         concat("&nbsp;".html_safe)
         concat(t("common.show"))
