8f429f893b1540e6ec8043878627b47f0b39f38e
Karsten Richter <kr@corika.com>
2026-03-09 22:19:52 +0000

    fix distinction invoice member account bookings

--- app/jobs/generate_distinction_invoice_job.rb
+++ app/jobs/generate_distinction_invoice_job.rb
@@ -17,14 +17,14 @@ class GenerateDistinctionInvoiceJob < BaseInvoicesJob
     pdf = invoice.gen_pdf
     sepa = invoice.gen_sepa_file
 
-    booking_txt = "Ehrungsrechung #{invoice.number}"
-    booking = MemberAccountBooking.new_distinction_invoice(booking_txt, -1 * invoice.sum, invoice.customer.customer_id, pdf)
+    booking_txt = "Ehrungsrechung #{invoice.full_number}"
+    booking = MemberAccountBooking.new_distinction_invoice(booking_txt, -1 * invoice.total, invoice.customer.customer_id, pdf)
     booking.member_id = orchestra.member.id
     booking.invoice_id = invoice.id.to_s
     booking.save
 
     if invoice.customer.direct_debit?
-      @wdbooking = MemberAccountBooking.new_dd("Lastschrift #{booking_txt}", invoice.sum, sepa.orig_filename)
+      @wdbooking = MemberAccountBooking.new_dd("Lastschrift #{booking_txt}", invoice.total, sepa.orig_filename)
       @wdbooking.member_id = orchestra.member.id
       @wdbooking.invoice_id = invoice.id
       @wdbooking.save
