61a5d5591456c3a85247df3e954fb6a0093fe438
Karsten Richter <kr@corika.com>
2026-03-09 22:14:05 +0000

    improve ef event cards flow
    - enable retry on fail
    - state machine to prevent going back after payment

--- app/controllers/ef/event_cards_controller.rb
+++ app/controllers/ef/event_cards_controller.rb
@@ -7,23 +7,73 @@ module Ef
 
     before_action :set_event_card
 
+    def invalid_state
+
+    end
+
     def order_form
-      @event_card = EventCard.new
-      @event_card.country_code = "DE"
+      @event_card = event_card_from_session
+
+      if @event_card.present?
+        Rails.logger.debug("EC session ref: #{@event_card.checkout_reference}")
+        invoice = @event_card.to_invoice
+
+        if invoice.pdf_filename.present?
+          @event_card = nil
+          set_checkout_reference(nil)
+        end
+      end
+
+      if @event_card.nil?
+        @event_card = EventCard.new
+        @event_card.country_code = "DE"
+      end
+
+      @url = order_ef_event_cards_path(format: :html)
+
       @prices = BDZ_SETTINGS["festival_prices"]
     end
 
+    def update
+      @event_card = event_card_from_session
+
+      if @event_card.order_state == 99
+        redirect_to invalid_state_ef_event_cards_path
+        return
+      end
+
+      @event_card.update(event_card_params)
+      if @event_card.save
+        redirect_to choose_payment_ef_event_card_path(@event_card)
+      else
+        render partial: "order_form", status: :unprocessable_entity
+      end
+    end
+
     def order
-      @event_card = EventCard.new(event_card_params)
+      @event_card = event_card_from_session
+
+      if @event_card.nil?
+        session[:checkout_reference] =nil
+        @event_card = EventCard.new(event_card_params)
+      else
+        @event_card.update(event_card_params)
+      end
+
+      if @event_card.checkout_reference.nil?
+        uuid = SecureRandom.uuid
+        @event_card.checkout_reference = uuid
+        set_checkout_reference(uuid)
+      end
+
       @prices = BDZ_SETTINGS["festival_prices"]
 
       @event_card.festival_year = BDZ_SETTINGS["config"]["festival_year"]
       @event_card.orderdate = Time.zone.now
 
-      uuid = SecureRandom.uuid
-      @event_card.checkout_reference = uuid
 
       if @event_card.save
+        session[:checkout_reference] = @event_card.checkout_reference
         redirect_to choose_payment_ef_event_card_path(@event_card)
       else
         respond_to do |format|
@@ -57,22 +107,34 @@ module Ef
 
     def choose_payment
       @event_card = EventCard.find_by_checkout_reference(params[:checkout_reference])
+      redirected = check_order_state(@event_card)
+
+      return if redirected
+
+      set_checkout_reference(@event_card.checkout_reference)
+
+      if session[:checkout_reference].nil?
+        session[:checkout_reference] = @event_card.checkout_reference
+      end
+
       @invoice = @event_card.to_invoice
 
       respond_to do |format|
           format.html
-        end
-
+      end
     end
 
     def payment
       @event_card = EventCard.find_by_checkout_reference(params[:checkout_reference])
-      @event_card.update(event_card_params)
 
-      Rails.logger.debug(event_card_params)
-      Rails.logger.debug(@event_card.payment_method)
-      Rails.logger.debug(@event_card.valid?)
-      @event_card.save
+      if @event_card.order_state == 99
+        redirect_to invalid_state_ef_event_cards_path
+        return
+      end
+
+      @event_card.assign_attributes(event_card_params)
+
+      @event_card.update_invoice.save
 
       if @event_card.payment_method == "credit_card"
         cc_payment(@event_card)
@@ -83,16 +145,26 @@ module Ef
 
     def confirm_dd_payment
       @event_card = EventCard.find_by_checkout_reference(params[:checkout_reference])
+      @event_card.payment_method = "direct_debit"
       @event_card.update(event_card_params)
 
-      invoice = @event_card.to_invoice
-      invoice.save
-      @event_card.invoice_id = invoice.id
+      if @event_card.valid?
+        Rails.logger.debug("event card is valid in dd confirm")
+        invoice = @event_card.to_invoice
+        invoice.save
+        @event_card.invoice_id = invoice.id
+      else
+          respond_to do |format|
+          format.turbo_stream { render action: "payment", status: :unprocessable_entity }
+          end
+          return
+      end
 
       if @event_card.save
           redirect_to payment_complete_ef_event_card_path(@event_card)
           return
       else
+          Rails.logger.error(@event_card.errors)
           format.html { render action: "dd_payment", status: :unprocessable_entity }
       end
 
@@ -101,7 +173,25 @@ module Ef
     def payment_complete
       @event_card = EventCard.find_by_checkout_reference(params[:checkout_reference])
 
-      EventCardOrderProcessingJob.perform_later(params[:checkout_reference])
+      client = CorikaSumup::Client.new
+      checkouts = client.get_checkouts(@event_card.checkout_reference)
+
+      if checkouts.length >0
+        checkout = checkouts.first
+
+        @status = checkout["status"]
+      end
+
+      Rails.logger.debug("Payment complete: #{@status}")
+      #redirected = check_order_state(@event_card)
+      #return if redirected
+      if status == "PAID"
+        @event_card.order_state = 99
+        @event_card.save
+        # remove checkout reference from session
+        session[:checkout_reference] = nil
+        EventCardOrderProcessingJob.perform_later(params[:checkout_reference])
+      end
 
       respond_to do |format|
           format.html
@@ -178,8 +268,41 @@ module Ef
         :nr_fest, :nr_fest_erm, :nr_fest_bdz, :nr_fest_bdz_erm, :nr_do, :nr_do_erm, :nr_fr, :nr_fr_erm, :nr_sa, :nr_sa_erm, :nr_concert_so, :nr_concert_so_erm, :iban, :bic, :account_owner, :bank_name, :payment_method)
     end
 
+    private
     def set_event_card
       @event_card = EventCard.find_by(checkout_reference: params[:checkout_reference])
     end
+
+    def event_card_from_session
+      checkout_ref = session[:checkout_reference]
+
+      event_card = nil
+
+      if not checkout_ref.nil?
+        Rails.logger.debug("Found stored checkout reference: #{checkout_ref}")
+        event_card = EventCard.find_by(checkout_reference: checkout_ref)
+      end
+
+      event_card
+    end
+
+    def set_checkout_reference(ref)
+      session[:checkout_reference] = ref
+    end
+
+    private
+    def check_order_state(event_card)
+      case event_card.order_state
+        when 2
+          return false
+        when 99
+          redirect_to invalid_state_ef_event_cards_path
+          return true
+        else
+        return false
+      end
+      return false
+    end
   end
+
 end
