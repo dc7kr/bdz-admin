49298ca73349c102c9fd17abd7a03a662efd3c07
Karsten Richter <kr@corika.com>
2026-03-10 15:33:38 +0000

    fix ticket helper to use tax_rate not vat keyword argument

--- app/helpers/festival_ticket_helper.rb
+++ app/helpers/festival_ticket_helper.rb
@@ -2,7 +2,7 @@ module FestivalTicketHelper
 def consider_regular_tickets(invoice, count)
     prices = BDZ_SETTINGS["festival_prices"]
 
-    item = invoice.consider_item(count, prices["ticket_entry"]+prices["ticket_food"]+prices["ticket_drink"], I18n.t("event_card.fest"), vat: 7)
+    item = invoice.consider_item(count, prices["ticket_entry"]+prices["ticket_food"]+prices["ticket_drink"], I18n.t("event_card.fest"), tax_rate: 7)
 
     if item.present?
       item.item_taxes.clear()
@@ -17,7 +17,7 @@ end
 def consider_reduced_tickets(invoice, count)
     prices = BDZ_SETTINGS["festival_prices"]
 
-    item = invoice.consider_item(count, prices["ticket_entry_red"]+prices["ticket_food"]+prices["ticket_drink"], I18n.t("event_card.fest_erm"), vat: 7)
+    item = invoice.consider_item(count, prices["ticket_entry_red"]+prices["ticket_food"]+prices["ticket_drink"], I18n.t("event_card.fest_erm"), tax_rate: 7)
     if item.present?
       item.item_taxes.clear()
       item.add_split_tax(7,prices["ticket_food"], I18n.t("event_card.food_part"))
