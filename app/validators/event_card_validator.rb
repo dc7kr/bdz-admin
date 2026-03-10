24ded0e27971b6e4b98331a0925ba7fc1665af16
Karsten Richter <kr@corika.com>
2026-03-10 13:26:34 +0000

    conditional event card validation:
    - for dd payment, IBAN, BIC, Account Owner and Bank name are mandatory

--- app/validators/event_card_validator.rb
+++ app/validators/event_card_validator.rb
@@ -1,7 +1,26 @@
 class EventCardValidator < ActiveModel::Validator
   def validate(record)
-    return if record.total_card_count.positive?
 
-    record.errors.add(:base, :total_card_count_zero)
+    if not record.total_card_count.present? or record.total_card_count ==0
+      record.errors.add(:base, :total_card_count_zero)
+      return
+    end
+
+    if record.direct_debit?
+      presence_or_error(record,:iban)
+      presence_or_error(record,:bic)
+      presence_or_error(record,:bank_name)
+      presence_or_error(record,:account_owner)
+    end
+  end
+
+  private
+  def presence_or_error(record,sym)
+    if not record[sym].present?
+      record.errors.add(sym,:blank)
+      false
+    end
+
+    true
   end
 end
