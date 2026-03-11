425a72d367044fda8d486e2ea74b0dd400f978a2
Karsten Richter <kr@corika.com>
2026-03-11 19:14:27 +0000

    new download helper
    - generate invoice PDF and sepa path
    - create download path for archive files

---
+++ app/helpers/downloads_helper.rb
@@ -0,0 +1,23 @@
+module DownloadsHelper
+
+  def invoice_pdf_download_path(invoice)
+    return unless invoice.present?
+
+    pdf_file = invoice.get_invoice_file
+
+    archive_file_download_path(pdf_file)
+  end
+
+  def invoice_sepa_download_path(invoice)
+    return unless invoice.present? and invoice.sepa_filename.present?
+
+    # TODO: refactor invoice to provide get_sepa_file
+    sepa_file = invoice.gen_sepa_file
+
+    archive_file_download_path(sepa_file)
+  end
+
+  def archive_file_download_path(archive_file)
+    dl_path(archive_file.archive_folder, archive_file.orig_filename)
+  end
+end
