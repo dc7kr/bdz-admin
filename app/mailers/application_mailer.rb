bcfed97f276e177363eeee6146e9acc34d80e4d7
Karsten Richter <kr@corika.com>
2026-03-17 17:13:12 +0000

    add set_triggered_by common method

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,6 +2,8 @@ class ApplicationMailer < ActionMailer::Base
   default from: BDZ_SETTINGS["contacts"]["system"]["email"]
   layout "mailer"
 
+  attr_accessor :triggered_by
+
   def get_download_url(archive_file)
     Rails.application.routes.url_helpers.dl_url(year: archive_file.archive_folder, filename: archive_file.orig_filename)
   end
@@ -46,5 +48,13 @@ class ApplicationMailer < ActionMailer::Base
 
   def invoice_out_bcc
     BDZ_SETTINGS["invoice_config"]["invoice_out_bcc"]
+  end
+
+  def set_triggered_by(triggered_by_id)
+    if triggered_by_id.present? and triggered_by_id > 0 
+      self.triggered_by = User.find(triggered_by_id)
+    else
+      self.triggered_by = nil
+    end
   end
 end
