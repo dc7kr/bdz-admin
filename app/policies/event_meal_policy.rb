2f53d238d69fd63d2daa156ac63589ffd9b9b9c3
Karsten Richter <kr@corika.com>
2026-04-07 06:24:19 +0000

    allow arrival overview for festival permission

--- app/policies/event_meal_policy.rb
+++ app/policies/event_meal_policy.rb
@@ -1,2 +1,14 @@
 class EventMealPolicy < FestivalDataPolicy
+  def arrival_overview?
+    national_permission or user.has_role? :festival
+  end
+
+
+  class Scope < FestivalDataPolicy::Scope
+    def resolve
+      if national_permission? or user.has_role? :festival
+        scope.all
+      end
+    end
+  end
 end
