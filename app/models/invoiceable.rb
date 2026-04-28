1ded2ff5abfda10b33ae8ab0127d37b9ef1a1543
Karsten Richter <kr@corika.com>
2026-04-28 11:42:43 +0000

    add storno_invoice to invoiceable class

--- app/models/invoiceable.rb
+++ app/models/invoiceable.rb
@@ -12,4 +12,14 @@ class Invoiceable < ApplicationRecord
   def has_invoice?
     invoice_id.present?
   end
+
+  def storno_invoice
+    if not storno_invoice_id.present?
+      return nil
+    end
+
+    i = CorikaInvoices::Invoice.find(storno_invoice_id)
+
+    i
+  end
 end
