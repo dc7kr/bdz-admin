3163fd70fc5c9f6f5cadca95f3847e19abf61985
Karsten Richter <kr@corika.com>
2026-04-28 11:06:39 +0000

    update participant sheet
    - add signature field
    - add meal times
    - rework performance fields to a table

--- app/pdfs/participant_sheet_pdf.rb
+++ app/pdfs/participant_sheet_pdf.rb
@@ -29,7 +29,7 @@ class ParticipantSheetPdf < Prawn::Document
       text "#{I18n.t('event_meal.arrival_time')} #{@view.l app.event_meal.arrival_time}"
     end
 
-    move_down 20
+    move_down 10
     text "#{app.contact_person.fullname}", size: 14, style: :bold
     text "#{I18n.t('contact_person.phone')}: #{app.contact_person.phone}", size: 14, style: :bold
 
@@ -62,7 +62,7 @@ class ParticipantSheetPdf < Prawn::Document
     rows << [ appl.tickets, I18n.t("festival_application.tickets")]
     rows << [ appl.tickets_red, I18n.t("festival_application.tickets_red")]
 
-    move_down 20
+    move_down 10
     text I18n.t("participant_sheet.tickets"), style: :bold, size: 14
 
     if @appl.soloist_tickets.present? && @appl.soloist_tickets.positive?
@@ -79,27 +79,37 @@ class ParticipantSheetPdf < Prawn::Document
       columns(3).align = :right
     end
 
-    move_down 20
+    move_down 10
   end
 
   def performance(app)
     move_down 20
-    text I18n.t("participant_sheet.performance"), style: :bold
+    text I18n.t("participant_sheet.performance"), style: :bold, size: 14
 
-    text "#{I18n.t('festival_application.num_players')}: #{app.num_players}"
     concert = app.festival_concert
+
+    rows = []
+    rows << [ I18n.t('festival_application.num_players'), "#{app.num_players}" ]
 
     if concert.nil?
       text "#{I18n.t('festival_application.festival_concert_id')}: N/A"
     else
-      text "#{I18n.t('festival_application.festival_concert_id')}: #{app.festival_concert.title} #{@view.l app.festival_concert.event_time}"
-      text "#{I18n.t('festival_concert.location')}: #{app.festival_concert.location}"
+      rows << [ I18n.t('festival_application.festival_concert_id'), "#{app.festival_concert.title} #{@view.l app.festival_concert.event_time}" ]
+      rows << [ I18n.t('festival_concert.location'), app.festival_concert.location ]
+      rows << [ I18n.t('festival_application.rehearsal_time'), "#{app.rehearsal_time.localtime.strftime('%H:%M')}" ] unless app.rehearsal_time.nil?
+      rows << [ I18n.t('festival_application.stage_time'), "#{app.stage_time.strftime('%H:%M')}" ] unless app.stage_time.nil?
+      rows << [I18n.t('festival_application.equipment'), "#{app.equipment}" ]
+      rows << [I18n.t('festival_application.comment'), "#{app.comment}" ]
+
+      table rows do
+        cells.borders = []
+        cells.style(padding: 2)
+        columns(0).font_style = :bold
+        columns(0).align = :right
+        columns(1).align = :left
+      end
     end
 
-    text "#{I18n.t('festival_application.rehearsal_time')}: #{app.rehearsal_time.localtime.strftime('%H:%M')}" unless app.rehearsal_time.nil?
-    text "#{I18n.t('festival_application.stage_time')}: #{app.stage_time.strftime('%H:%M')}" unless app.stage_time.nil?
-    text "#{I18n.t('festival_application.equipment')}: #{app.equipment}"
-    text "#{I18n.t('festival_application.comment')}: #{app.comment}"
   end
 
   def food(app)
@@ -110,13 +120,14 @@ class ParticipantSheetPdf < Prawn::Document
       text "KEINE ESSENSMELDUNG!", style: :bold, size: 20
       restore_stroke_and_fill
     else
-      text I18n.t("participant_sheet.food"), style: :bold
+      text I18n.t("participant_sheet.food"), style: :bold, size: 14
+
+      meal = app.event_meal
 
       rows = []
-
-      rows << [ app.event_meal.veg, I18n.t('event_meal.veg') ]
-      rows << [ app.event_meal.tln-app.event_meal.veg, I18n.t('event_meals.regular_meals') ] 
-      rows << [ app.event_meal.tln, I18n.t('participant_sheet.meals') ]
+      rows << [ meal.veg, I18n.t('event_meals.veg') ]
+      rows << [ meal.tln-meal.veg, I18n.t('event_meals.regular_meals') ]
+      rows << [ meal.tln, I18n.t('participant_sheet.meals') ]
 
       table rows do
         cells.borders = []
@@ -124,6 +135,27 @@ class ParticipantSheetPdf < Prawn::Document
         columns(0).font_style = :bold
         columns(0).align = :right
         columns(1).align = :left
+      end
+
+      move_down 20
+
+      text I18n.t("event_meals.meal_times"), style: :bold, size: 14
+
+      rows = []
+      rows << [ "", I18n.t("event_meals.lunch"), I18n.t("event_meals.dinner") ]
+      rows << [ I18n.t('date.day_names')[4], I18n.t("event_meals.l_do")[meal.lunch1-1], I18n.t("event_meals.d_do")[meal.dinner1-1] ]
+      rows << [ I18n.t('date.day_names')[5], I18n.t("event_meals.l_fr")[meal.lunch2-1], I18n.t("event_meals.d_fr")[meal.dinner2-1] ]
+      rows << [ I18n.t('date.day_names')[6], I18n.t("event_meals.l_sa")[meal.lunch3-1], I18n.t("event_meals.d_sa")[meal.dinner3-1] ]
+
+      table rows do
+        cells.borders = []
+        cells.style(padding: 2)
+
+        rows(0).font_style = :bold
+        columns(0).font_style = :bold
+        columns(0).align = :right
+        columns(1).align = :left
+        columns(2).align = :left
       end
     end
   end
@@ -134,9 +166,21 @@ class ParticipantSheetPdf < Prawn::Document
     text "#{I18n.t(label)} #{value}"
   end
 
+  def signature(app)
+    move_down 30
+    text I18n.t("festival_applications.confirm_ticket_received"), style: :bold
+    move_down 30
+        font_size 12
+        text("_______________________________________")
+        font_size 12
+        text(app.contact_person.fullname)
+
+  end
+
   def body(app)
     invoice(app)
     food(app)
     performance(app)
+    signature(app)
   end
 end
