5d77d0655dfaae365ea608c4ba8eff4162ad1869
Karsten Richter <kr@corika.com>
2026-05-31 10:46:10 +0000

    fix delete button not working after turbo request
    The document entites have to be resolved in the confirm method itself.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -12,11 +12,24 @@ import "channels/notification_channel"
 
 import { Turbo } from "@hotwired/turbo-rails"
 
-const dialog = document.getElementById("turbo-confirm-dialog")
-const messageElement = document.getElementById("turbo-confirm-message")
-const confirmButton = dialog?.querySelector("button[value='confirm']")
+Turbo.StreamActions.console_log = function() {
+  const message = this.getAttribute("message")
+  console.log(message)
+}
+
+Turbo.StreamActions.show_toast = function() {
+  const id = this.getAttribute("id")
+  var toastEL = document.getElementById(id)
+  bootstrap.Toast.getOrCreateInstance(toastEL).show()
+}
+
 
 Turbo.config.forms.confirm = (message, element, submitter) => {
+
+  let dialog = document.getElementById("turbo-confirm-dialog")
+  let messageElement = document.getElementById("turbo-confirm-message")
+  let confirmButton = dialog?.querySelector("button[value='confirm']")
+
   // Fall back to native confirm if dialog isn't in the DOM
   if (!dialog) return Promise.resolve(confirm(message))
 
